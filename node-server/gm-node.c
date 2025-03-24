b32ef4debfa3b7f49ee6a58eac5824276b62b3d7
James Cave <jcave137@gmail.com>
2025-03-23 20:35:17 -0400

    handle SIGINT

--- node-server/gm-node.c
+++ node-server/gm-node.c
@@ -10,34 +10,57 @@
 #include <poll.h>
 #include <string.h>
 #include <fcntl.h>
-#include <signal.h>
 
 #include <mosquitto.h>
 
 #include "gm-node.h"
 
+// vscode sucks
+#ifndef __USE_POSIX
+#define __USE_POSIX
+#endif
+#include <signal.h>
+
 void exit_cleanup(void) {
     // clean up job scripts
     // TODO: this is a horrendous hack
-    // at the very least, it should be done when there are no running jobs anymore
-    // not at exit
-    system("rm " TEMP_PREFIX "*");
+    // at the very least, it should be done when a job exits and no jobs
+    // remain, not at exit
+    puts("rm -f " TEMP_PREFIX "*");     // to remind the world of my sins
+    system("rm -f " TEMP_PREFIX "*");
 }
 
 // TODO: actually wire this up
 void sigint_cleanup(int signum) {
-    gm_shutdown();
+    if (signum == SIGINT) {
+        fprintf(stderr, "\nshutting down due to SIGINT...\n");
+        gm_shutdown();
+    }
+    else {
+        fprintf(stderr, "signal handler called on unexpected signal %d\n", signum);
+        abort();
+    }
 }
 
 int main(int argc, char *const *argv) {
-    setbuf(stdout, NULL);
+    // install exit handler
     atexit(exit_cleanup);
 
-    init_job_table();
+    // install SIGINT handler (which ends up calling the exit handler)
+    struct sigaction sa;
+    sa.sa_handler = sigint_cleanup;
+    sigset_t sa_set;
+    sigemptyset(&sa_set);
+    sa.sa_mask = sa_set;
+    sa.sa_flags = 0;
+    if (sigaction(SIGINT, &sa, NULL) != 0) {
+        err(1, "could not set signal handler");
+    }
 
+    // start up the subsystems and do an event loop
+    init_job_table();
     gm_init_mqtt();
     gm_connect_mqtt();
-
     for(;;) {
         gm_do_events();
     }
