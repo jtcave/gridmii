8a65a2bae0a2ecbbcba646f79ab162357ed125b4
James Cave <jcave137@gmail.com>
2025-05-23 10:59:24 -0400

    node server: use catch-all subscription to node topics This reduces the number of subscriptions we get billed for

--- node-server/mqtt.c
+++ node-server/mqtt.c
@@ -111,26 +111,12 @@ void subscribe_topics() {
     // buffer for topic string
     char topic_buf[512];
 
-    // subscribe to job submit endpoint
+    // subscribe to node endpoint pattern
     int rv;
-    snprintf(topic_buf, sizeof(topic_buf), "%s/submit/+", gm_config.node_name);
+    snprintf(topic_buf, sizeof(topic_buf), "%s/#", gm_config.node_name);
     rv = mosquitto_subscribe(gm_mosq, NULL, topic_buf, 2);
     if (rv != MOSQ_ERR_SUCCESS) {
-        errx(1, "could not subscribe, mosq_err_t = %d (%s)", rv, mosquitto_strerror(rv));
-    }
-
-    // subscribe to scram endpoint
-    snprintf(topic_buf, sizeof(topic_buf), "%s/scram", gm_config.node_name);
-    rv = mosquitto_subscribe(gm_mosq, NULL, topic_buf, 2);
-    if (rv != MOSQ_ERR_SUCCESS) {
-        errx(1, "could not subscribe, mosq_err_t = %d (%s)", rv, mosquitto_strerror(rv));
-    }
-
-    // subscribe to exit endpoint
-    snprintf(topic_buf, sizeof(topic_buf), "%s/exit", gm_config.node_name);
-    rv = mosquitto_subscribe(gm_mosq, NULL, topic_buf, 2);
-    if (rv != MOSQ_ERR_SUCCESS) {
-        errx(1, "could not subscribe, mosq_err_t = %d (%s)", rv, mosquitto_strerror(rv));
+        errx(1, "could not subscribe to node topics, mosq_err_t = %d (%s)", rv, mosquitto_strerror(rv));
     }
 }
 
