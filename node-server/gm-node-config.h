381feffd797a03fdad1dc7a68461b628f5adf9b7
James Cave <jcave137@gmail.com>
2025-06-08 19:39:11 -0400

    node server: bump up output buffer size

--- node-server/gm-node-config.h
+++ node-server/gm-node-config.h
@@ -11,7 +11,7 @@
 #define GRID_PORT_DEFAULT 1883
 
 // buffer size for subprocess stdout/stderr reads
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 1024
 
 // used as a millisecond delay value in poll(), etc.
 #define DELAY_MS 100
