a3ea71ea2b6c4067e775a85f0637abaaafc4154a
James Cave <jcave137@gmail.com>
2025-06-24 19:31:54 -0400

    bot: begin to put commands into cogs

--- discord-bot/gridbot/gridbot.py
+++ discord-bot/gridbot/gridbot.py
@@ -1,10 +1,9 @@
 import asyncio
 import logging
-import discord.ext.commands
 from discord import Message
+from discord.ext import commands
 from discord.ext.commands import Context, errors
 import aiomqtt
-from discord.ext.commands._types import BotT
 
 from .config import *
 from .entity import Job, Node
@@ -21,7 +20,7 @@ class FlexBot(discord.ext.commands.Bot):
 
     # we want flex commands, we also want to swallow some exceptions
 
-    async def on_command_error(self, context: Context[BotT], exception: errors.CommandError, /) -> None:
+    async def on_command_error(self, context: Context, exception: errors.CommandError, /) -> None:
         # Swallow exceptions that are due to user error and are not supposed to be serious issues
         if isinstance(exception, errors.CheckFailure):
             logging.debug("global command check failed")
@@ -37,13 +36,13 @@ class FlexBot(discord.ext.commands.Bot):
         else:
             await super().on_command_error(context, exception)
 
-    async def invoke(self, ctx: Context[BotT], /) -> None:
+    async def invoke(self, ctx: Context, /) -> None:
         try:
             await super().invoke(ctx)
         except errors.CommandNotFound:
             await self.flex_command(ctx)
 
-    async def flex_command(self, ctx: Context[BotT], /):
+    async def flex_command(self, ctx: Context, /):
         """Run when a non-existent command is attempted"""
         raise NotImplementedError("flex command not specified")
 
@@ -64,20 +63,6 @@ class FlexBot(discord.ext.commands.Bot):
         raise NotImplementedError("flex reply not specified")
 
 
-def job_for_reply(ctx: Context) -> Job|None:
-    """Attempt to find a job based on what message the user is replying to.
-    Returns None if the job is gone or there was no job in the first place"""
-    msg = ctx.message
-    if msg.type != discord.MessageType.reply:
-        return None
-    replied_msg_id = msg.reference.message_id
-    # scan for messages
-    for job in Job.table.values():
-        if job.output_message.id == replied_msg_id:
-            return job
-    # no message
-    return None
-
 class GridMiiBot(FlexBot):
     """Discord client that accepts GridMii commands and processes MQTT messages"""
     def __init__(self, *, intents: discord.Intents):
@@ -95,6 +80,10 @@ class GridMiiBot(FlexBot):
         self.mqtt_task = self.loop.create_task(self.do_mqtt_task())
         # Install the "after broker connection" task"
         self.after_broker_connect_task = self.loop.create_task(self.after_broker_connect())
+        # Install cogs
+        cogs = (UserCommandCog, AdminCommandCog, JobControlCog, NeofetchCog)
+        for cog_class in cogs:
+            await self.add_cog(cog_class(self))
 
     async def after_broker_connect(self):
         # Wait for the event to fire
@@ -258,13 +247,14 @@ class GridMiiBot(FlexBot):
         await job.stdin(payload, self.mq_client)
 
 
-    async def flex_command(self, ctx: Context[BotT], /):
+    async def flex_command(self, ctx: Context, /):
         # chop off the command prefix
         command_string = ctx.message.content[1:]
         await self.submit_job(ctx, command_string)
 
     async def flex_reply(self, ctx: Context, /):
-        job = job_for_reply(ctx)
+        # XXX: this method should probably be in that cog itself
+        job = JobControlCog.job_for_reply(ctx)
         if job is not None:
             await self.stdin_post(ctx, job)
 
@@ -275,133 +265,155 @@ def check_channel(ctx: Context) -> bool:
     """If a channel was specified in the config, check to see if the command was sent in that channel."""
     return ctx.channel.id == CHANNEL or CHANNEL is None
 
-@bot.command(name="yougood")
-async def ping(ctx: Context):
-    """Check connectivity to broker"""
-    if bot.mq_client is None:
-        await ctx.reply(":-1: mq_client is None")
-    elif bot.mq_client._disconnected.done():
-        # XXX: don't grovel into internal members like that
-        await ctx.reply(":-1: mq_client._disconnected has come to pass")
-    else:
-        await ctx.reply(":+1:")
-
-@bot.command()
-async def nodes(ctx: Context):
-    """View available nodes"""
-    message = '\n'.join(f"* {n}" for n in Node.table) if Node.table else "No nodes are online"
-    await ctx.message.reply(content=message)
-
-@bot.command()
-async def locus(ctx: Context, new_locus: str|None=None):
-    """Manually set the locus node for new jobs"""
-
-    if new_locus is None:
-        if Node.locus is None:
-            content = "No node is currently set to run commands.\nOne will be selected when the next command is sent."
+class UserCommandCog(commands.Cog, name="User Commands"):
+    """Cog for GridMii commands regular users can use"""
+    @commands.command(name="yougood")
+    async def ping(self, ctx: Context):
+        """Check connectivity to broker"""
+        if bot.mq_client is None:
+            await ctx.reply(":-1: mq_client is None")
+        elif bot.mq_client._disconnected.done():
+            # XXX: don't grovel into internal members like that
+            await ctx.reply(":-1: mq_client._disconnected has come to pass")
         else:
-            content = f"Commands are being sent to {Node.locus}"
-    elif new_locus in Node.table:
-        Node.locus = new_locus
-        content = f":+1: Commands will now run on {new_locus}"
-    else:
-        content = f":x: The node {new_locus} is not in the node table."
-    await ctx.reply(content)
+            await ctx.reply(":+1:")
 
-@bot.command()
-async def scram(ctx: Context):
-    """Terminate all jobs across the entire grid"""
-    logging.warning("scram command called")
-    try:
-        await bot.mq_client.publish("grid/scram")
-    except aiomqtt.MqttError as ex_mq:
-        logging.exception("error publishing scram")
-        await ctx.message.reply(f"**Couldn't send scram request**: {str(ex_mq)}")
-    else:
-        await ctx.message.reply(":+1: wait for the jobs to complete")
+    @commands.command()
+    async def nodes(self, ctx: Context):
+        """View available nodes"""
+        message = '\n'.join(f"* {n}" for n in Node.table) if Node.table else "No nodes are online"
+        await ctx.message.reply(content=message)
 
-@bot.command()
-async def jobs(ctx: Context):
-    """View running jobs"""
-    def _line(job: Job):
-        return f"* #{job.jid}, on `{job.target_node}`, see {job.output_message.jump_url}"
-    if Job.table:
-        table = '\n'.join(_line(j) for j in Job.table.values())
-    else:
-        table = "No jobs running"
-    await ctx.message.reply(table)
+    @commands.command()
+    async def locus(self, ctx: Context, new_locus: str|None=None):
+        """Manually set the locus node for new jobs"""
 
-@bot.command()
-async def reload(ctx: Context, node_name:str|None=None):
-    """Instruct a node to reload its server (useful for updates)"""
-    if node_name is None:
-        await ctx.reply(f"`node_name` parameter required\nfor example: `$reload {Node.locus}`?")
-        return
-    node = Node.table.get(node_name, None)
-    if node is None:
-        await ctx.reply(f"node {node_name} is not in the node table")
-    else:
-        await node.reload(bot.mq_client)
-    # no need to send an ack reply because the node should induce connect/disconnect
+        if new_locus is None:
+            if Node.locus is None:
+                content = "No node is currently set to run commands.\nOne will be selected when the next command is sent."
+            else:
+                content = f"Commands are being sent to {Node.locus}"
+        elif new_locus in Node.table:
+            Node.locus = new_locus
+            content = f":+1: Commands will now run on {new_locus}"
+        else:
+            content = f":x: The node {new_locus} is not in the node table."
+        await ctx.reply(content)
 
+    @commands.command()
+    async def jobs(self, ctx: Context):
+        """View running jobs"""
 
-# neofetch hack
+        def _line(job: Job):
+            return f"* #{job.jid}, on `{job.target_node}`, see {job.output_message.jump_url}"
+
+        if Job.table:
+            table = '\n'.join(_line(j) for j in Job.table.values())
+        else:
+            table = "No jobs running"
+        await ctx.message.reply(table)
+
+class AdminCommandCog(commands.Cog, name="Admin Commands"):
+    """Cog for commands only admins can use"""
+
+    @commands.command()
+    async def scram(self, ctx: Context):
+        """Terminate all jobs across the entire grid"""
+        logging.warning("scram command called")
+        try:
+            await bot.mq_client.publish("grid/scram")
+        except aiomqtt.MqttError as ex_mq:
+            logging.exception("error publishing scram")
+            await ctx.message.reply(f"**Couldn't send scram request**: {str(ex_mq)}")
+        else:
+            await ctx.message.reply(":+1: wait for the jobs to complete")
+
+    @commands.command()
+    async def reload(self, ctx: Context, node_name:str|None=None):
+        """Instruct a node to reload its server (useful for updates)"""
+        if node_name is None:
+            await ctx.reply(f"`node_name` parameter required\nfor example: `$reload {Node.locus}`?")
+            return
+        node = Node.table.get(node_name, None)
+        if node is None:
+            await ctx.reply(f"node {node_name} is not in the node table")
+        else:
+            await node.reload(bot.mq_client)
+        # no need to send an ack reply because the node should induce connect/disconnect
+
 FETCH_SCRIPT = """
 fastfetch --pipe false -s none
 echo '===snip==='
 fastfetch --pipe false -l none -s 'Title:Separator:OS:Host:Kernel:Uptime:Packages:CPU:Memory:Swap:Disk:LocalIp:Locale:Break'
 """
 assert len(FETCH_SCRIPT) < 2000     # discord message size
+class NeofetchCog(commands.Cog):
+    """Cog for the $neofetch override"""
+    @commands.command()
+    async def neofetch(self, ctx: Context):
+        """Run fastfetch, then rearrange the output to look correct"""
+        await bot.submit_job(ctx, FETCH_SCRIPT, fastfetch_filter)
 
-@bot.command()
-async def neofetch(ctx: Context):
-    """Run fastfetch, then rearrange the output to look correct"""
-    await bot.submit_job(ctx, FETCH_SCRIPT, fastfetch_filter)
+class JobControlCog(discord.ext.commands.Cog, name="Job Control"):
+    """Cog that contains commands to interact with a running job"""
 
-# commands to interact with a running job
+    @staticmethod
+    def job_for_reply(ctx: Context) -> Job | None:
+        """Attempt to find a job based on what message the user is replying to.
+        Returns None if the job is gone or there was no job in the first place"""
+        msg = ctx.message
+        if msg.type != discord.MessageType.reply:
+            return None
+        replied_msg_id = msg.reference.message_id
+        # scan for messages
+        for job in Job.table.values():
+            if job.output_message.id == replied_msg_id:
+                return job
+        # no message
+        return None
 
-@bot.command()
-async def jobinfo(ctx: Context):
-    """Report information about a given job, which is specified by replying to the job"""
-    job = job_for_reply(ctx)
-    if job is not None:
-        await ctx.reply(repr(job))
+    @commands.command()
+    async def jobinfo(self, ctx: Context):
+        """Report information about a job"""
+        job = self.job_for_reply(ctx)
+        if job is not None:
+            await ctx.reply(repr(job))
 
-@bot.command()
-async def eof(ctx: Context):
-    """Send end-of-file to a job's stdin"""
-    job = job_for_reply(ctx)
-    if job is not None:
-        await job.eof(bot.mq_client)
+    @commands.command()
+    async def eof(self, ctx: Context):
+        """Send end-of-file to a job's stdin"""
+        job = self.job_for_reply(ctx)
+        if job is not None:
+            await job.eof(bot.mq_client)
 
-@bot.command()
-async def signal(ctx: Context, signal_num: int):
-    """Send a signal (numeric code) to a job"""
-    job = job_for_reply(ctx)
-    if job is not None:
-        await job.signal(signal_num, bot.mq_client)
-        await ctx.reply(f"Sent signal {signal_num} to the job")
+    @commands.command()
+    async def signal(self, ctx: Context, signal_num: int):
+        """Send a signal (numeric code) to a job"""
+        job = self.job_for_reply(ctx)
+        if job is not None:
+            await job.signal(signal_num, bot.mq_client)
+            await ctx.reply(f"Sent signal {signal_num} to the job")
 
-@bot.command()
-async def kill(ctx: Context):
-    """Send SIGKILL to a job"""
-    await signal(ctx, 9)    # SIGKILL is 9 on all platforms I can see
+    @commands.command()
+    async def kill(self, ctx: Context):
+        """Send SIGKILL to a job"""
+        await self.signal(ctx, 9)    # SIGKILL is 9 on all platforms I can see
 
-@bot.command(name="ctrl-c")
-async def ctrlc(ctx: Context):
-    """Send SIGINT to a job, like pressing Ctrl-C"""
-    await signal(ctx, 2)    # SIGINT is 2 on all platforms I can see
+    @commands.command(name="ctrl-c")
+    async def ctrlc(self, ctx: Context):
+        """Send SIGINT to a job, like pressing Ctrl-C"""
+        await self.signal(ctx, 2)    # SIGINT is 2 on all platforms I can see
 
-@bot.command()
-async def jobtail(ctx: Context, lines:int=5):
-    """Show the last few lines of a job's output"""
-    job = job_for_reply(ctx)
-    if job is not None:
-        # add 1 to `lines` because there's probably a blank line at the end, and the user won't be counting that
-        buffer_tail = '\n'.join(job.tail(lines+1))
-        # TODO: escape output
-        output = f"```ansi\n{buffer_tail}\n```"
-        if len(output) > 2000:
-            # Ideally we'd
-            output = f"***Output too large***\nThe message would have been {len(output)} characters long, but only 2000 are allowed"
-        await ctx.reply(output)
+    @commands.command()
+    async def jobtail(self, ctx: Context, lines:int=5):
+        """Show the last few lines of a job's output"""
+        job = self.job_for_reply(ctx)
+        if job is not None:
+            # add 1 to `lines` because there's probably a blank line at the end, and the user won't be counting that
+            buffer_tail = '\n'.join(job.tail(lines+1))
+            # TODO: escape output
+            output = f"```ansi\n{buffer_tail}\n```"
+            if len(output) > 2000:
+                # Ideally we'd lower the parameter until it fits...
+                output = f"***Output too large***\nThe message would have been {len(output)} characters long, but only 2000 are allowed"
+            await ctx.reply(output)
