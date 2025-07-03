028b39c83356fb855f88e426347d528c56f6ba88
James Cave <jcave137@gmail.com>
2025-07-03 17:56:44 -0400

    bot: pull command cogs out into separate files

--- discord-bot/gridbot/gridbot.py
+++ discord-bot/gridbot/gridbot.py
@@ -1,15 +1,14 @@
 import asyncio
+import aiomqtt
 import logging
 
-import discord.ext.commands
 from discord import Message
-from discord.ext import commands
-from discord.ext.commands import Context, errors
-import aiomqtt
+from discord.ext.commands import errors, Context
 
 from .config import *
 from .entity import Job, Node
-from .output_filter import fastfetch_filter
+from .grid_cmd import DEFAULT_COGS, JobControlCog
+from .neofetch import NeofetchCog
 
 
 ## discord part ##
@@ -82,7 +81,7 @@ class GridMiiBot(FlexBot):
         # Install the "after broker connection" task"
         self.after_broker_connect_task = self.loop.create_task(self.after_broker_connect())
         # Install cogs
-        cogs = (UserCommandCog, AdminCommandCog, JobControlCog, NeofetchCog)
+        cogs = DEFAULT_COGS + (NeofetchCog,)
         for cog_class in cogs:
             await self.add_cog(cog_class(self))
 
@@ -265,185 +264,3 @@ bot = GridMiiBot(intents=bot_intents)
 def check_channel(ctx: Context) -> bool:
     """If a channel was specified in the config, check to see if the command was sent in that channel."""
     return ctx.channel.id == CHANNEL or CHANNEL is None
-
-class UserCommandCog(commands.Cog, name="User Commands"):
-    """Cog for GridMii commands regular users can use"""
-    def __init__(self, bot: GridMiiBot):
-        self.bot = bot
-
-    @commands.command(name="yougood")
-    async def ping(self, ctx: Context):
-        """Check connectivity to broker"""
-        if self.bot.mq_client is None:
-            await ctx.reply(":-1: mq_client is None")
-        elif self.bot.mq_client._disconnected.done():
-            # XXX: don't grovel into internal members like that
-            await ctx.reply(":-1: mq_client._disconnected has come to pass")
-        else:
-            await ctx.reply(":+1:")
-
-    @commands.command()
-    async def nodes(self, ctx: Context):
-        """View available nodes"""
-        message = '\n'.join(f"* {n}" for n in Node.table) if Node.table else "No nodes are online"
-        await ctx.message.reply(content=message)
-
-    @commands.command()
-    async def locus(self, ctx: Context, new_locus: str|None=None):
-        """Manually set the locus node for new jobs"""
-        if new_locus is None:
-            if Node.locus is None:
-                content = "No node is currently set to run commands.\nOne will be selected when the next command is sent."
-            else:
-                content = f"Commands are being sent to {Node.locus}"
-        elif new_locus in Node.table:
-            Node.locus = new_locus
-            content = f":+1: Commands will now run on {new_locus}"
-        else:
-            content = f":x: The node {new_locus} is not in the node table."
-        await ctx.reply(content)
-
-    @commands.command()
-    async def jobs(self, ctx: Context):
-        """View running jobs"""
-        def _line(job: Job):
-            return f"* #{job.jid}, on `{job.target_node}`, see {job.output_message.jump_url}"
-
-        if Job.table:
-            table = '\n'.join(_line(j) for j in Job.table.values())
-        else:
-            table = "No jobs running"
-        await ctx.message.reply(table)
-
-class AdminCommandCog(commands.Cog, name="Admin Commands"):
-    """Cog for commands only admins can use"""
-    def __init__(self, bot: GridMiiBot):
-        self.bot = bot
-
-    async def cog_check(self, ctx: Context) -> bool:
-        user_roles = [r.id for r in ctx.author.roles]
-        for admin_role in ADMIN_ROLES:
-            if admin_role in user_roles:
-                return True
-        logging.info(f"admin command denied for user '{ctx.author.display_name}' ({ctx.author.id})")
-        return False
-
-    @commands.command()
-    async def scram(self, ctx: Context):
-        """Terminate all jobs across the entire grid"""
-        logging.warning("scram command called")
-        try:
-            await self.bot.mq_client.publish("grid/scram")
-        except aiomqtt.MqttError as ex_mq:
-            logging.exception("error publishing scram")
-            await ctx.message.reply(f"**Couldn't send scram request**: {str(ex_mq)}")
-        else:
-            await ctx.message.reply(":+1: wait for the jobs to complete")
-
-    @commands.command()
-    async def reload(self, ctx: Context, node_name:str|None=None):
-        """Instruct a node to reload its server (useful for updates)"""
-        if node_name is None:
-            await ctx.reply(f"`node_name` parameter required\nfor example: `$reload {Node.locus}`?")
-            return
-        node = Node.table.get(node_name, None)
-        if node is None:
-            await ctx.reply(f"node {node_name} is not in the node table")
-        else:
-            await node.reload(self.bot.mq_client)
-        # no need to send an ack reply because the node should disconnect and reconnect
-
-    @commands.command()
-    async def eject(self, ctx: Context, node_name:str|None=None):
-        """Eject a node from the grid.
-        WARNING: if jobs are running, output will be lost"""
-        if node_name is None:
-            await ctx.reply(f"`node_name` parameter required")
-            return
-        node = Node.table.get(node_name, None)
-        if node is None:
-            await ctx.reply(f"node {node_name} is not in the node table")
-        else:
-            await node.eject(self.bot.mq_client)
-        # no need to send an ack reply because the node should disconnect
-
-FETCH_SCRIPT = """
-fastfetch --pipe false -s none
-echo '===snip==='
-fastfetch --pipe false -l none -s 'Title:Separator:OS:Host:Kernel:Uptime:Packages:CPU:Memory:Swap:Disk:LocalIp:Locale:Break'
-"""
-assert len(FETCH_SCRIPT) < 2000     # discord message size
-class NeofetchCog(commands.Cog):
-    """Cog for the $neofetch override"""
-    def __init__(self, bot: GridMiiBot):
-        self.bot = bot
-
-    @commands.command()
-    async def neofetch(self, ctx: Context):
-        """Run fastfetch, then rearrange the output to look correct"""
-        await bot.submit_job(ctx, FETCH_SCRIPT, fastfetch_filter)
-
-class JobControlCog(discord.ext.commands.Cog, name="Job Control"):
-    """Cog that contains commands to interact with a running job"""
-    def __init__(self, bot: GridMiiBot):
-        self.bot = bot
-
-    @staticmethod
-    def job_for_reply(ctx: Context) -> Job | None:
-        """Attempt to find a job based on what message the user is replying to.
-        Returns None if the job is gone or there was no job in the first place"""
-        msg = ctx.message
-        if msg.type != discord.MessageType.reply:
-            return None
-        replied_msg_id = msg.reference.message_id
-        # scan for messages
-        for job in Job.table.values():
-            if job.output_message.id == replied_msg_id:
-                return job
-        # no message
-        return None
-
-    @commands.command()
-    async def jobinfo(self, ctx: Context):
-        """Report information about a job"""
-        job = self.job_for_reply(ctx)
-        if job is not None:
-            await ctx.reply(repr(job))
-
-    @commands.command()
-    async def eof(self, ctx: Context):
-        """Send end-of-file to a job's stdin"""
-        job = self.job_for_reply(ctx)
-        if job is not None:
-            await job.eof(self.bot.mq_client)
-
-    @commands.command()
-    async def signal(self, ctx: Context, signal_num: int):
-        """Send a signal (numeric code) to a job"""
-        job = self.job_for_reply(ctx)
-        if job is not None:
-            await job.signal(signal_num, self.bot.mq_client)
-            await ctx.reply(f"Sent signal {signal_num} to the job")
-
-    @commands.command()
-    async def kill(self, ctx: Context):
-        """Send SIGKILL to a job"""
-        await self.signal(ctx, 9)    # SIGKILL is 9 on all platforms I can see
-
-    @commands.command(name="ctrl-c")
-    async def ctrlc(self, ctx: Context):
-        """Send SIGINT to a job, like pressing Ctrl-C"""
-        await self.signal(ctx, 2)    # SIGINT is 2 on all platforms I can see
-
-    @commands.command()
-    async def jobtail(self, ctx: Context, lines:int=5):
-        """Show the last few lines of a job's output"""
-        job = self.job_for_reply(ctx)
-        if job is not None:
-            # add 1 to `lines` because there's probably a blank line at the end, and the user won't be counting that
-            buffer_tail = '\n'.join(job.tail(lines+1))
-            output = f"```ansi\n{buffer_tail}\n```"
-            if len(output) > 2000:
-                # Ideally we'd lower the parameter until it fits...
-                output = f"***Output too large***\nThe message would have been {len(output)} characters long, but only 2000 are allowed"
-            await ctx.reply(output)
---
+++ discord-bot/gridbot/neofetch.py
@@ -0,0 +1,107 @@
+import re
+from itertools import zip_longest
+import discord.ext.commands as commands
+
+from .output_filter import BACKTICKS_ZWS
+
+FETCH_SCRIPT = """
+fastfetch --pipe false -s none
+echo '===snip==='
+fastfetch --pipe false -l none -s 'Title:Separator:OS:Host:Kernel:Uptime:Packages:CPU:Memory:Swap:Disk:LocalIp:Locale:Break'
+"""
+assert len(FETCH_SCRIPT) < 2000     # discord message size
+class NeofetchCog(commands.Cog):
+    """Cog for the $neofetch override"""
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command()
+    async def neofetch(self, ctx: commands.Context):
+        """Run fastfetch, then rearrange the output to look correct"""
+        await self.bot.submit_job(ctx, FETCH_SCRIPT, fastfetch_filter)
+
+def fastfetch_filter(s: str) -> str:
+    """Massage fastfetch output into something Discord likes"""
+    # nasty maze of regexes
+    # big thanks to Techflash for making a prototype of this
+
+    # split logo and info
+    SEP = "===snip==="
+    if SEP in s:
+        logo, info = s.split(SEP)
+    else:
+        logo = s
+        info = ""
+
+    # clean logo
+    # Remove all non-color codes at the start and end
+    logo = re.sub(r'^\x1B\[\?\d+[hl]+', '', logo)
+    logo = re.sub(r'\x1B\[19A\x1B\[9999999D.*$', '', logo, flags=re.DOTALL)
+    # Remove all non-color ANSI escape sequences except color codes
+    logo = re.sub(r'\x1B\[[0-9;]*[A-HJKST]', '', logo)
+    logo = logo.rstrip()
+
+    # clean info
+    if info:
+        # Remove all non-color codes at the start
+        info = re.sub(r'^\x1B\[\?\d+[hl]+', '', info)
+        # Remove trailing non-color codes and blank lines
+        info = re.sub(r'\x1B\[\?\d+[hl]+$', '', info, flags=re.DOTALL)
+        info = info.rstrip()
+        # Remove all non-color ANSI escape sequences except color codes
+        info = re.sub(r'\x1B\[[0-9;]*[A-HJKST]', '', info)
+
+    # combine horizontally
+    if not info:
+        return logo
+    else:
+        logo_lines = logo.splitlines()
+        info_lines = info.splitlines()
+        # Determine the maximum width of the logo without ANSI codes
+        max_logo_width = max(len(re.sub(r'\x1B\[[0-9;]*m', '', line)) for line in logo_lines)
+        ansi_color_re = re.compile(r'(\x1B\[[0-9;]*m)')
+        def _combine():
+            last_color = ""
+            first_line = False
+            for logo_part, info_part in zip_longest(logo_lines, info_lines, fillvalue=""):
+                # XXX: some lines to this effect were in the original code Techflash sent me
+                # if not logo_part: break
+                # Extract last color code in the logo line
+                color_codes = ansi_color_re.findall(logo_part)
+                if color_codes:
+                    # XXX: Don't apply if reset
+                    if color_codes[0] != "\x1b[0m" or len(color_codes) != 1:
+                        last_color = ''.join(color_codes)
+
+                # Reapply last_color to the current line
+                if first_line:
+                    first_line = False
+                elif not re.match(r'^\s*\x1B\[[0-9;]*m', logo_part):
+                    logo_part = last_color + logo_part
+
+                # Put info line to the right of the logo line, padding as needed
+                combined_line = f"{logo_part}{' ' * (max_logo_width - len(re.sub(r'\x1B\[[0-9;]*m', '', logo_part)) + 4)}{info_part}"
+                # XXX: last minute cleanup
+                combined_line = combined_line.replace("\x1b[?25l", "")
+                combined_line = combined_line.replace("\x1b[?25h", "")
+                combined_line = combined_line.replace("\x1b[?7l", "")
+                combined_line = combined_line.replace("\x1b[m", "\x1b[0m")
+                combined_line = combined_line.replace("\x1b[0m\x1b[0m", "\x1b[0m")
+                for i in range(1, 9):
+                    combined_line = combined_line.replace(f"\x1b[9{i}m", f"\x1b[1m\x1b[3{i}m")
+                combined_line = re.sub(r'\x1B]8;;.*\x1B\\/', '/', combined_line)
+                combined_line = combined_line.replace("\x1b]8;;\x1b\\", "")
+
+                # XXX: if we hit triple backticks we lose our codeblock
+                combined_line = combined_line.replace("```", BACKTICKS_ZWS)
+
+                # XXX: combined_line.rstrip() doesn't work to remove whitespace :(
+                while combined_line[-1] == ' ':
+                    combined_line = combined_line[:-1]
+
+                if combined_line.endswith("\x1b[0m"):
+                    combined_line = combined_line[:-len("\x1b[0m")]
+
+                yield combined_line
+        # end def _combine
+        return '\n'.join(_combine())
