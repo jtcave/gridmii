36252de3d60b3ce35be4cca37a504c77ed4cd839
James Cave <jcave137@gmail.com>
2025-10-22 15:19:54 -0400

    bot: !upload command

--- discord-bot/gridbot/grid_cmd.py
+++ discord-bot/gridbot/grid_cmd.py
@@ -287,4 +287,33 @@ class AutoRollCallCog(GridMiiCogBase):
             logging.info("performing roll call")
             await self.mq_client.publish("grid/roll_call", qos=2)
 
-DEFAULT_COGS = (UserCommandCog, AdminCommandCog, JobControlCog, AutoRollCallCog)
+class FileTransferCog(GridMiiCogBase):
+    UPLOAD_SCRIPT = """
+    if command -v curl > /dev/null
+    then
+      echo Downloading:
+      echo '{0}'
+      curl -Os '{0}'
+    else
+      echo Please install curl, then download this url:
+      echo '{0}'
+    fi
+    """
+
+    @commands.command()
+    async def upload(self, ctx: Context):
+        """Upload the attached file to your current node. Requires curl to be installed"""
+        attachments = ctx.message.attachments
+        if not attachments:
+            await ctx.reply(":x: You need to attach one or more files")
+            return
+        elif len(attachments) > 1:
+            await ctx.reply(":x: Currently only one file at a time can be uploaded")
+
+        attachment, = attachments
+
+        script = self.UPLOAD_SCRIPT.format(attachment.url)
+        await self.bot.submit_job(ctx, script)
+
+
+DEFAULT_COGS = (UserCommandCog, AdminCommandCog, JobControlCog, FileTransferCog, AutoRollCallCog)
